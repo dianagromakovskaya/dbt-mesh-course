a2d341dd739aca19b2e0c2be863d39d1a617e0b3
Diana Gromakovskaia <dianagr1996@gmail.com>
2025-02-05 18:08:41 +0000

    added fct_orders v2

---
+++ models/marts/fct_orders_v2.sql
@@ -0,0 +1,52 @@
+with 
+
+orders as (
+    
+    select * from {{ ref('stg_orders')}}
+
+),
+
+order_items as (
+    
+    select * from {{ ref('order_items')}}
+
+),
+
+order_items_summary as (
+
+    select
+
+        order_items.order_id,
+
+        sum(supply_cost) as order_cost,
+        sum(is_food_item) as count_food_items,
+        sum(is_drink_item) as count_drink_items
+
+
+    from order_items
+
+    group by 1
+
+),
+
+
+compute_booleans as (
+    select
+
+        orders.order_id,
+        orders.location_id,
+        orders.customer_id,
+        orders.order_total as order_amount,
+        orders.tax_paid,
+       cast(orders.ordered_at as date) ordered_at,
+
+        count_food_items > 0 as is_food_order,
+        count_drink_items > 0 as is_drink_order,
+        order_cost
+
+    from orders
+    
+    left join order_items_summary on orders.order_id = order_items_summary.order_id
+)
+
+select * from compute_booleans
